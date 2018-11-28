8a208bc01e987d921023e810a49686e4ae003aac
Harshita Girase <harshitagirase97@gmail.com>
2018-11-28 03:29:56 -0500

    Fixed stuff

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__) #instance of the Flask class
+mode = 'default'
+red_1 = '0'
+green_1 = '0'
+blue_1 = '200'
+red_2 = '255'
+green_2 = '200'
+blue_2 = '0'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', mode = mode, red_1 = red_1, green_1 = green_1, blue_1 = blue_1, red_2 = red_2, green_2 = green_2, blue_2 = blue_2 )
 
 
 @app.route('/data')
 def data_endpoint():
-    return '$fade@80,127,255:255,10,60:'
+    return '$'+mode+'@'+red_1+','+green_1+','+blue_1+':'+red_2+','+green_2+','+blue_2+':'
+
+
+@app.route('/form', methods=['POST']) #allow POST requests
+def result():
+    mode = request.form('mode')
+    red_1 = request.form('red_1')
+    green_1 = request.form('green_1')
+    blue_1 = request.form('blue_1')
+    red_2 = request.form('red_2')
+    green_2 = request.form('green_2')
+    blue_2 = request.form('blue_2')
+    return str(request.form)
 
 
 if __name__ == '__main__':
