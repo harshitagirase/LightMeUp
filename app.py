a66fedcb1e6a9c8c273cb479160c496aa1e03acc
angus <angushtlam@gmail.com>
2018-11-28 03:02:53 -0500

    Fixed Heroku port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__) #instance of the Flask class
@@ -14,7 +15,7 @@ def data_endpoint():
 
 if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0')
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
 
 
 # get and post route for getting and updating some config on the server that the feather can pull from and update values for the fastled library
